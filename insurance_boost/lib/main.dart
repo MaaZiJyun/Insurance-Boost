3bd985143214d076a44757506f4cff024c7d1863
WANG YIDA <ciscolevisama@gmail.com>
2022-02-18 18:46:43 +0800

    RecomendList
    update RecomendList

--- insurance_boost/lib/main.dart
+++ insurance_boost/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:insurance_boost/global/global_variables.dart' as globals;
 import 'package:insurance_boost/pages/home/home_page.dart';
 import 'package:insurance_boost/pages/home_welcome.dart';
 import 'package:insurance_boost/pages/index_page.dart';
+import 'package:insurance_boost/pages/recomended_list_page.dart';
 import 'package:insurance_boost/pages/reward_page.dart';
 import 'package:insurance_boost/pages/settings_page.dart';
 import 'package:insurance_boost/pages/splash_page.dart';
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       initialRoute: '/',
       routes: {
-        '/': (context) => SplashPage(),
+        '/': (context) => RecomendList(),
         '/WelcomeScreen': (context) => WelcomeScreen(),
         '/LoginScreen': (context) => LoginScreen(),
         '/SignupScreen': (context) => SignupScreen(),
