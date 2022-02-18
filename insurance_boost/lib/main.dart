20dbce89aa18c671a4344eb0838724e8cff574b6
MA ZHIYUAN <201464@student.upm.edu.my>
2022-02-18 16:36:00 +0800

    Update main.dart

--- insurance_boost/lib/main.dart
+++ insurance_boost/lib/main.dart
@@ -8,7 +8,6 @@ import 'package:insurance_boost/global/global_variables.dart' as globals;
 import 'package:insurance_boost/pages/home/home_page.dart';
 import 'package:insurance_boost/pages/home_welcome.dart';
 import 'package:insurance_boost/pages/index_page.dart';
-import 'package:insurance_boost/pages/package_page.dart';
 import 'package:insurance_boost/pages/reward_page.dart';
 import 'package:insurance_boost/pages/settings_page.dart';
 import 'package:insurance_boost/pages/splash_page.dart';
@@ -39,7 +38,7 @@ class MyApp extends StatelessWidget {
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       initialRoute: '/',
       routes: {
-        '/': (context) => PackagePage(),
+        '/': (context) => SplashPage(),
         '/WelcomeScreen': (context) => WelcomeScreen(),
         '/LoginScreen': (context) => LoginScreen(),
         '/SignupScreen': (context) => SignupScreen(),
