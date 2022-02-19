5c6ea132f1dc6686101dcebe323b11d8edcdc33d
WANG YIDA <ciscolevisama@gmail.com>
2022-02-19 12:03:33 +0800

    two class

---
+++ insurance_boost/lib/models/package.dart
@@ -0,0 +1,9 @@
+class Package {
+  final String Id;
+  final String detial;
+  final double price;
+  final String categories;
+  final int point;
+
+  Package(this.Id, this.detial, this.price, this.categories, this.point);
+}
