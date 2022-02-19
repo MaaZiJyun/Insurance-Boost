0db011b756fbaa6fbf042a887ba2a473adbc8080
WANG YIDA <ciscolevisama@gmail.com>
2022-02-19 22:11:14 +0800

    google login

--- insurance_boost/lib/pages/auth/login_screen.dart
+++ insurance_boost/lib/pages/auth/login_screen.dart
@@ -33,149 +33,153 @@ class _LoginScreenState extends State<LoginScreen> {
   bool _value = false;
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 13),
-            child: Center(
+    return ChangeNotifierProvider(
+        create: (context) => GoogleSignInProvider(),
+        child: SafeArea(
+          child: Scaffold(
+            body: SingleChildScrollView(
               child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    customText(
-                        txt: "Login Now",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 26,
-                        )),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    customText(
-                        txt: "Please login to continue using our app.",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 14,
-                        )),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    customText(
-                        txt: "Enter via social networks",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 14,
-                        )),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    SocialLoginButton(
-                      buttonType: SocialLoginButtonType.google,
-                      onPressed: () {
-                        final provider = Provider.of<GoogleSignInProvider>(
-                            context,
-                            listen: false);
-                        provider.googleLogin();
-                      },
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    SocialLoginButton(
-                      buttonType: SocialLoginButtonType.facebook,
-                      onPressed: () {},
-                    ),
-
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    customText(
-                        txt: "or login with email",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 14,
-                        )),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const SizedBox(height: 20),
-                    CustomTextField(
-                        controller: emailController,
-                        Lone: "Email",
-                        Htwo: "Email"),
-                    const SizedBox(height: 20),
-                    CustomTextField(
-                        controller: passwordController,
-                        Lone: "Password",
-                        Htwo: "Password"),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Checkbox(
-                          value: _value,
-                          onChanged: (newValue) {
-                            setState(() {
-                              _value = newValue!;
-                            });
-                            const Text(
-                              "Remember me",
-                              style: TextStyle(
-                                  fontSize: 13, color: AppColors.kBlackColor),
-                            );
+                padding: const EdgeInsets.only(top: 13),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        customText(
+                            txt: "Login Now",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 26,
+                            )),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        customText(
+                            txt: "Please login to continue using our app.",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 14,
+                            )),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        customText(
+                            txt: "Enter via social networks",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 14,
+                            )),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        SocialLoginButton(
+                          buttonType: SocialLoginButtonType.google,
+                          onPressed: () {
+                            final provider = Provider.of<GoogleSignInProvider>(
+                                context,
+                                listen: false);
+                            provider.googleLogin();
                           },
                         ),
-                        Spacer(),
-                        const TextButton(
-                          onPressed: null,
-                          child: Text(
-                            "Forgot password?",
-                            style: TextStyle(
-                              fontSize: 15,
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        SocialLoginButton(
+                          buttonType: SocialLoginButtonType.facebook,
+                          onPressed: () {},
+                        ),
+
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        customText(
+                            txt: "or login with email",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 14,
+                            )),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        const SizedBox(height: 20),
+                        CustomTextField(
+                            controller: emailController,
+                            Lone: "Email",
+                            Htwo: "Email"),
+                        const SizedBox(height: 20),
+                        CustomTextField(
+                            controller: passwordController,
+                            Lone: "Password",
+                            Htwo: "Password"),
+                        const SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: _value,
+                              onChanged: (newValue) {
+                                setState(() {
+                                  _value = newValue!;
+                                });
+                                const Text(
+                                  "Remember me",
+                                  style: TextStyle(
+                                      fontSize: 13,
+                                      color: AppColors.kBlackColor),
+                                );
+                              },
                             ),
+                            Spacer(),
+                            const TextButton(
+                              onPressed: null,
+                              child: Text(
+                                "Forgot password?",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(height: 40),
+                        ElevatedButton(
+                          onPressed: login,
+                          child: Text('Log in'),
+                          style: ElevatedButton.styleFrom(
+                            shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(40.0),
+                            ),
+                            primary: Colors.teal,
+                            onPrimary: Colors.white,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 100, vertical: 15),
                           ),
                         ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        InkWell(
+                          child: RichText(
+                            text: RichTextSpan(
+                                one: "Don’t have an account ? ",
+                                two: "Sign Up"),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const SignupScreen()));
+                          },
+                        ),
+                        //Text("data"),
                       ],
                     ),
-
-                    const SizedBox(height: 40),
-                    ElevatedButton(
-                      onPressed: login,
-                      child: Text('Log in'),
-                      style: ElevatedButton.styleFrom(
-                        shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(40.0),
-                        ),
-                        primary: Colors.teal,
-                        onPrimary: Colors.white,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    InkWell(
-                      child: RichText(
-                        text: RichTextSpan(
-                            one: "Don’t have an account ? ", two: "Sign Up"),
-                      ),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const SignupScreen()));
-                      },
-                    ),
-                    //Text("data"),
-                  ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 
   Future login() async {
--- insurance_boost/lib/provider/google_sign_in.dart
+++ insurance_boost/lib/provider/google_sign_in.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+// import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
