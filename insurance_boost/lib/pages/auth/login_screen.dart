ab15925e4558476691b096dfdabb85611784782a
WANG YIDA <ciscolevisama@gmail.com>
2022-02-20 11:32:00 +0800

    change

--- insurance_boost/lib/pages/auth/login_screen.dart
+++ insurance_boost/lib/pages/auth/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
@@ -243,10 +242,6 @@ class _LoginScreenState extends State<LoginScreen> {
 
     // Once signed in, return the UserCredential
     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
-    await FirebaseFirestore.instance
-        .collection('user')
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .get();
   }
   //   FirebaseAuth _auth = FirebaseAuth.instance;
   //   static final FacebookLogin facebookSignIn = new FacebookLogin();
