451fe4d6c573e3eb6cb7006de68ad7c3f085c9a1
MA ZHIYUAN <201464@student.upm.edu.my>
2022-02-20 11:06:16 +0800

    package chnages

--- insurance_boost/lib/global/global_variables.dart
+++ insurance_boost/lib/global/global_variables.dart
@@ -1,6 +1,9 @@
 library my_prj.globals;
 
 import 'package:flutter/material.dart';
+import 'package:insurance_boost/models/user.dart';
 
 bool NIGHT_MODE = false;
 bool NOTE_DELIVER = true;
+
+Person? me;
--- insurance_boost/lib/pages/detail_pages/submission_detail_page.dart
+++ insurance_boost/lib/pages/detail_pages/submission_detail_page.dart
@@ -1,182 +1,239 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:insurance_boost/api/user_api.dart';
+import 'package:insurance_boost/models/user.dart';
 import 'package:insurance_boost/pages/detail_pages/pdf_detail_page.dart';
 
 class SubmissionDetailPage extends StatefulWidget {
-  const SubmissionDetailPage({Key? key}) : super(key: key);
+  final String author;
+  final String title;
+  final String detail;
+  final String email;
+  final String report;
+  final String submission;
+  final Timestamp date;
+
+  String me = '';
+
+  SubmissionDetailPage(
+      {Key? key,
+      required this.author,
+      required this.title,
+      required this.detail,
+      required this.email,
+      required this.report,
+      required this.submission,
+      required this.date})
+      : super(key: key);
 
   @override
   _SubmissionDetailPageState createState() => _SubmissionDetailPageState();
 }
 
 class _SubmissionDetailPageState extends State<SubmissionDetailPage> {
+  Future<void> getUser() async {
+    await FirebaseFirestore.instance
+        .collection('user')
+        .doc(widget.author)
+        .get()
+        .then((value) {
+      widget.me = value['username'];
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'My Submission',
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        elevation: 1,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.teal,
-          ),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'This is TITLE',
+    return FutureBuilder(
+        future: getUser(),
+        builder: (context, snapshot) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                'My Submission',
                 style: TextStyle(
-                  fontSize: 35,
-                  color: Colors.teal,
-                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
               ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                children: [
-                  Text(
-                    'Status: ',
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                  StatusTag(
-                    color: Colors.red,
-                    text: 'Done',
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Uploaded by: USERNAME',
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                'Date: 2020.12.13',
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                'Email: 12001u@maks.com',
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Details: This is the example of the introduction of this submission. and all basic elements have been designed on the page.',
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(40.0),
-                  ),
-                  backgroundColor: Colors.teal,
-                  primary: Colors.white,
-                ),
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+              elevation: 1,
+              leading: IconButton(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PDFViewerPage(
-                        url:
-                            'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+                  Navigator.of(context).pop();
+                },
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.teal,
+                ),
+              ),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(30),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.title,
+                      style: TextStyle(
+                        fontSize: 35,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  );
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.attach_file_sharp),
                     SizedBox(
-                      width: 10,
+                      height: 30,
                     ),
-                    Text('My Submission File'),
+                    Row(
+                      children: [
+                        Text(
+                          'Status: ',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        StatusTag(
+                          text:
+                              widget.report == '' ? 'Processing' : 'Completed',
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Uploaded by: ${widget.me}',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Date: ${widget.date.toDate().month}-${widget.date.toDate().day}-${widget.date.toDate().year}',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Email: ${widget.email}',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Details: ${widget.detail}',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(40.0),
+                        ),
+                        backgroundColor: Colors.teal,
+                        primary: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PDFViewerPage(
+                              url: '${widget.submission}',
+                            ),
+                          ),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.attach_file_sharp),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('View my Submission File'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(40.0),
+                        ),
+                        backgroundColor:
+                            widget.report == '' ? Colors.grey : Colors.teal,
+                        primary: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PDFViewerPage(
+                              url: '${widget.submission}',
+                            ),
+                          ),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.book),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('Get report of submission'),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-              SizedBox(
-                height: 5,
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(40.0),
-                  ),
-                  backgroundColor: Colors.teal,
-                  primary: Colors.white,
-                ),
-                onPressed: () {},
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.book),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text('Report to My File'),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: Colors.teal,
-        icon: Icon(Icons.edit),
-        label: Text('Edit'),
-        onPressed: () {},
-      ),
-    );
+            ),
+            floatingActionButton: FloatingActionButton.extended(
+              backgroundColor: Colors.red,
+              icon: Icon(Icons.delete),
+              label: Text('Delete This'),
+              onPressed: () {},
+            ),
+          );
+        });
   }
 }
 
 class StatusTag extends StatelessWidget {
-  final Color color;
   final String text;
-  const StatusTag({Key? key, required this.color, required this.text})
-      : super(key: key);
+  late Color color;
+  StatusTag({Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    if (text == 'Processing') {
+      color = Colors.redAccent;
+    } else {
+      color = Colors.green;
+    }
     return Container(
       padding: const EdgeInsets.only(
         left: 10,
