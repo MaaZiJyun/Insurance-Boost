f1e2c9fa90474dfc79865e21b618ec06162925f0
WANG YIDA <ciscolevisama@gmail.com>
2022-02-20 22:47:39 +0800

    commit

--- insurance_boost/lib/pages/edit_profile_page.dart
+++ insurance_boost/lib/pages/edit_profile_page.dart
@@ -313,6 +313,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
     );
   }
 
+// noting?
   @override
   void dispose() {
     // Clean up the controller when the widget is removed from the
